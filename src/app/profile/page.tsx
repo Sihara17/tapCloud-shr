638097050a62143cdec3f8f5cad50539f3b58b65
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-08-07 20:59:28 +0700

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,31 +1,29 @@
 "use client";
 
-import {useWalletAccountStore} from "@/components/wallet/account/auth.hooks";
-import {useKaiaWalletSdk} from "@/components/wallet/sdk/walletSdk.hooks";
 import {useCallback} from "react";
+import {liff} from "@/utils/liff";
+import {useShareTargetPicker} from "@/components/invitation/Invitation.hooks";
 import styles from "./page.module.css";
 
-export default function Event () {
-    const { account, setAccount } = useWalletAccountStore();
-    const { disconnectWallet } = useKaiaWalletSdk();
-    const onDisconnectButtonClick = useCallback(()=>{
-        disconnectWallet().then(()=> {
-            setAccount(null);
-            sessionStorage.removeItem('ACCOUNT');
-        });
-    },[disconnectWallet, setAccount]);
+export default function Profile() {
+    const { mutateAsync: openShareTargetPicker } = useShareTargetPicker();
+
+    const onInvitationButtonClick = useCallback(() => {
+        if (liff.isInClient()) {
+            openShareTargetPicker();
+        } else {
+            navigator.clipboard
+                .writeText("https://dapp-starter.netlify.app")
+                .then(() => alert("URL Copy Success!"))
+                .catch(() => alert("URL Copy Failed!"));
+        }
+    }, [openShareTargetPicker]);
+
     return (
         <div className={styles.root}>
-            <div className={styles.body}>
-                {
-                    account ?
-                        <>
-                            <p>wallet address: {account.slice(0, 5) + '...' + account.slice(-3)}</p>
-                            <button className={styles.button} onClick={onDisconnectButtonClick}>disconnect</button>
-                        </>
-                        : <>need login</>
-                }
-            </div>
+            <button onClick={onInvitationButtonClick} className={styles.button}>
+                Invite Friends
+            </button>
         </div>
     );
 }
